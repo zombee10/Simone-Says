673c7b8e5f6af0c1242233b5cc291079cb0ee685
zombee10 <chikenpocalypse@gmail.com>
2016-09-21 10:48:18 -0400

    Add files via upload

---
+++ Simon Game/simon Game/simon Game/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace simon_Game
+{
+    partial class frmSimon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnblue = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnYelow = new System.Windows.Forms.Button();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.PatternLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(12, 12);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(100, 100);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "Red";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnblue
+            // 
+            this.btnblue.Location = new System.Drawing.Point(118, 12);
+            this.btnblue.Name = "btnblue";
+            this.btnblue.Size = new System.Drawing.Size(100, 100);
+            this.btnblue.TabIndex = 1;
+            this.btnblue.Text = "Blue";
+            this.btnblue.UseVisualStyleBackColor = true;
+            this.btnblue.Click += new System.EventHandler(this.btnblue_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Location = new System.Drawing.Point(12, 118);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(100, 100);
+            this.btnGreen.TabIndex = 2;
+            this.btnGreen.Text = "Green";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnYelow
+            // 
+            this.btnYelow.Location = new System.Drawing.Point(118, 118);
+            this.btnYelow.Name = "btnYelow";
+            this.btnYelow.Size = new System.Drawing.Size(100, 100);
+            this.btnYelow.TabIndex = 3;
+            this.btnYelow.Text = "Yellow";
+            this.btnYelow.UseVisualStyleBackColor = true;
+            this.btnYelow.Click += new System.EventHandler(this.btnYelow_Click);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Location = new System.Drawing.Point(235, 36);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(47, 13);
+            this.ScoreLabel.TabIndex = 4;
+            this.ScoreLabel.Text = "Score: 0";
+            // 
+            // PatternLabel
+            // 
+            this.PatternLabel.AutoSize = true;
+            this.PatternLabel.Location = new System.Drawing.Point(238, 98);
+            this.PatternLabel.Name = "PatternLabel";
+            this.PatternLabel.Size = new System.Drawing.Size(73, 13);
+            this.PatternLabel.TabIndex = 5;
+            this.PatternLabel.Text = "Items in Chain";
+            // 
+            // frmSimon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 246);
+            this.Controls.Add(this.PatternLabel);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.btnYelow);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnblue);
+            this.Controls.Add(this.btnRed);
+            this.Name = "frmSimon";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnblue;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnYelow;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label PatternLabel;
+    }
+}
+
---
+++ Simon Game/simon Game/simon Game/Form1.cs
@@ -0,0 +1,115 @@
+﻿//Programer John Monnin and Kayla Cole
+//09.21.16
+//VS C#
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace simon_Game
+{
+    public partial class frmSimon : Form
+    {
+        public frmSimon()//Starts up the form
+        {
+            InitializeComponent();
+        }
+
+        int onInList = 0;//List of the colors that are generated
+        List<int> pattern = new List<int>();//Starts a new list for a chain
+        Random rand = new Random();//Declares the random number generator seed
+        bool playingBack = false;//If the pattern is playing back or not
+     
+
+        private void btnRed_Click(object sender, EventArgs e)//Tests to see if you clicked the correct button
+        {
+            testCorrect(0);
+        }
+
+        private void btnblue_Click(object sender, EventArgs e)
+        {
+            testCorrect(1);
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            testCorrect(2);
+        }
+
+        private void btnYelow_Click(object sender, EventArgs e)
+        {
+            testCorrect(3);
+        }
+
+        void testCorrect(int color)//function that checks to see if the buttons are pressed in the corect order
+        {
+            if (playingBack)
+                return;
+            if (pattern[onInList] == color)//Checks to see if the color is on the list in the correct order
+            {
+                onInList++;//Updates the score and adds another color to the pattern
+            }
+            else
+            {
+                MessageBox.Show("You fail! Final Score: " + pattern.Count.ToString());//If you are incorrect on the pattern
+                onInList = 0;//Resets the list to 0
+                pattern = new List<int>();// Creates a new list
+                new Thread(playback).Start();//Starts
+            }
+            if (onInList >= pattern.Count)//Adds another color to the pattern
+            {
+                pattern.Add(rand.Next(0, 4));//Adds Red, Green, Yellow, Blue
+                onInList = 0;
+                new Thread(playback).Start();
+            }
+            ScoreLabel.Text = ("Score: " + pattern.Count.ToString());//Changes the score
+            PatternLabel.Text = ("Items within pattern: " + onInList.ToString());/// Adds the pattern to the chain
+        }
+
+
+        
+        void playback()//Lights up the buttons
+        {
+            playingBack = true;
+            foreach(int color in pattern)
+            {
+                switch(color)
+                {
+                    case 0:
+                        btnRed.BackColor = Color.Red;
+                        Thread.Sleep(200);
+                        btnRed.BackColor = Color.Transparent;
+                        break;
+                    case 1:
+                        btnblue.BackColor = Color.Blue;
+                        Thread.Sleep(200);
+                        btnblue.BackColor = Color.Transparent;
+                        break;
+                    case 2:
+                        btnGreen.BackColor = Color.Green;
+                        Thread.Sleep(200);
+                        btnGreen.BackColor = Color.Transparent;
+                        break;
+                    case 3:
+                        btnYelow.BackColor = Color.Yellow;
+                        Thread.Sleep(200);
+                        btnYelow.BackColor = Color.Transparent;
+                        break;
+                }
+                Thread.Sleep(200);
+            }
+            playingBack = false;    
+        }
+        private void Form1_Load(object sender, EventArgs e)//Starts the seed
+        {
+            pattern.Add(rand.Next(0, 4));
+            new Thread(playback).Start();
+        }
+    }
+}
